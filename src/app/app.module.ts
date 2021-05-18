e4deaba1e924227aba527582bc636a5db6c6de0a
Alexandre Silva <alexandre.silva@dsbsolucoes.com.br>
2021-05-18 16:21:35 -0300

    feat: directive ng if

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgStyleComponent } from './ng-style/ng-style.component';
@@ -8,10 +9,18 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
-import { NgClassComponent } from './ng-class/ng-class.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { SubNgIfComponent } from './ng-if/sub-ng-if/sub-ng-if.component';
 
 @NgModule({
-  declarations: [AppComponent, NgStyleComponent, NgClassComponent],
+  declarations: [
+    AppComponent,
+    NgStyleComponent,
+    NgClassComponent,
+    NgIfComponent,
+    SubNgIfComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -19,6 +28,8 @@ import { NgClassComponent } from './ng-class/ng-class.component';
     MatSelectModule,
     MatRadioModule,
     MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
